c62357f83f521166c4f7a2958b13ab64a94130b9
Greg Bremble <32407913+gbremble@users.noreply.github.com>
2018-07-19 23:33:48 -0400

    added SQL schema and seeds

---
+++ schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(128) NOT NULL,
+    department_name VARCHAR(64) NOT NULL,
+    price INT(10,2) NOT NULL,
+    stock_quantity INT
+);
+
+SELECT * FROM products;
---
+++ seeds.sql
@@ -0,0 +1,34 @@
+USE bamazon;
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Wheel Hub Assy', 'automotive', 82.89, 16);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Fuel Injector Assy', 'automotive', 220.79, 4);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Brake Pads, front', 'automotive', 24.79, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Brake Pads, rear', 'automotive', 25.79, 8);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Brake Rotor, front', 'automotive', 37.79, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Brake Rotor, rear', 'automotive', 36.79, 6);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Anker 5-in-1 USB-C Hub', 'electronics', 59.99, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Red Sparrow', 'books', 12.97, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('The Economics of Inequality', 'books', 16.10, 15);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('Anker Wireless Qi Charger', 'electronics', 13.99, 14);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ('MacBook Pro, 15-inch (mid 2018)', 'electronics', 6699.00, 4);
